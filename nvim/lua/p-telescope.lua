29d0e67ae1a577f1943b8756bc5452bd3fb274c1
henryvulcan <hongphongle115@gmail.com>
2023-11-06 03:20:53 +0700

    fix telescope

--- nvim/lua/p-telescope.lua
+++ nvim/lua/p-telescope.lua
@@ -1,33 +1,37 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>f', builtin.find_files, {})
-vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
+local builtin = require "telescope.builtin"
+vim.keymap.set("n", "<leader>f", builtin.find_files, {})
+vim.keymap.set("n", "<leader>g", builtin.live_grep, {})
 
-
-local actions = require('telescope.actions')
-require('nvim-web-devicons').setup({
+local actions = require "telescope.actions"
+require("nvim-web-devicons").setup {
   override = {},
-  default = true
-})
-require('telescope').setup {
+  default = true,
+}
+require("telescope").setup {
   pickers = {
     find_files = {
-      hidden = true
-    }
+      hidden = true,
+    },
   },
   defaults = {
-    file_ignore_patterns = { "node_modules", "yarn.lock","schema.gql" },
+    file_ignore_patterns = {
+      "lazy-lock.json",
+      "node_modules",
+      "yarn.lock",
+      "schema.gql",
+    },
     dynamic_preview_title = true,
-    path_display = { 'smart' },
+    path_display = { "smart" },
     mappings = {
       i = {
         ["<esc>"] = actions.close,
       },
-    }
+    },
   },
   layout_config = {
     horizontal = {
       preview_cutoff = 100,
-      preview_width = 0.5
-    }
-  }
+      preview_width = 0.5,
+    },
+  },
 }
