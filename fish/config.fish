b9096e3e167d9bc77c440cbc852fcc35c39202d9
phonglenus <hongphongle115@gmail.com>
2023-12-06 10:27:13 +0700

    feat: go env && rvm func

--- fish/config.fish
+++ fish/config.fish
@@ -28,3 +28,26 @@ end
 test -s "$HOME/.config/envman/load.fish"; and source "$HOME/.config/envman/load.fish"
 
 # starship init fish | source
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
+    eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+        . "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/opt/homebrew/Caskroom/miniconda/base/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
+
+# go environment
+set -x GOPATH ~/development/go
+set -x PATH $PATH $GOROOT/bin $GOPATH/bin
+
+set -gx PATH $PATH $HOME/.composer/vendor/bin
+
+#rvm
+rvm default
