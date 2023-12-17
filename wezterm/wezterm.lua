bac8fbd251f0f13f27ac466913704a784bb968ee
phonglenus <hongphongle115@gmail.com>
2023-12-17 17:40:28 +0700

    update config wez

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,6 +1,8 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
 
+local fish_path = "/opt/homebrew/bin/fish"
+
 local config = {}
 -- Use config builder object if possible
 if wezterm.config_builder then
@@ -8,16 +10,17 @@ if wezterm.config_builder then
 end
 
 -- Settings
+config.default_prog = { fish_path, "-l" }
+
 config.color_scheme = "Tokyo Night"
-config.font = wezterm.font_with_fallback({
-	{ family = "CaskaydiaCove Nerd Font", scale = 1.6 },
-	{ family = "FantasqueSansM Nerd Font", scale = 1.4 },
-})
+
+config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Medium" })
+config.font_size = 18
 config.window_background_opacity = 0.9
 config.window_decorations = "RESIZE"
 config.window_close_confirmation = "AlwaysPrompt"
 config.scrollback_lines = 3000
-config.default_workspace = "home"
+config.default_workspace = "main"
 
 -- Dim inactive panes
 config.inactive_pane_hsb = {
@@ -29,31 +32,54 @@ config.inactive_pane_hsb = {
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
 	-- Send C-a when pressing C-a twice
-	{ key = "a", mods = "LEADER", action = act.SendKey({ key = "a", mods = "CTRL" }) },
+	{ key = "a", mods = "LEADER|CTRL", action = act.SendKey({ key = "a", mods = "CTRL" }) },
 	{ key = "c", mods = "LEADER", action = act.ActivateCopyMode },
+	{ key = "phys:Space", mods = "LEADER", action = act.ActivateCommandPalette },
 
 	-- Pane keybindings
-	{ key = "-", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-	-- SHIFT is for when caps lock is on
-	{ key = "|", mods = "LEADER|SHIFT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "s", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "v", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
 	{ key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
 	{ key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
 	{ key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
 	{ key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
-	{ key = "x", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true }) },
+	{ key = "q", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true }) },
 	{ key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
-	{ key = "s", mods = "LEADER", action = act.RotatePanes("Clockwise") },
+	{ key = "o", mods = "LEADER", action = act.RotatePanes("Clockwise") },
 	-- We can make separate keybindings for resizing panes
 	-- But Wezterm offers custom "mode" in the name of "KeyTable"
-	{ key = "r", mods = "LEADER", action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }) },
+	{
+		key = "r",
+		mods = "LEADER",
+		action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }),
+	},
 
 	-- Tab keybindings
-	{ key = "n", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
+	{ key = "t", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
 	{ key = "[", mods = "LEADER", action = act.ActivateTabRelative(-1) },
 	{ key = "]", mods = "LEADER", action = act.ActivateTabRelative(1) },
-	{ key = "t", mods = "LEADER", action = act.ShowTabNavigator },
+	{ key = "n", mods = "LEADER", action = act.ShowTabNavigator },
+	{
+		key = "e",
+		mods = "LEADER",
+		action = act.PromptInputLine({
+			description = wezterm.format({
+				{ Attribute = { Intensity = "Bold" } },
+				{ Foreground = { AnsiColor = "Fuchsia" } },
+				{ Text = "Renaming Tab Title...:" },
+			}),
+			action = wezterm.action_callback(function(window, pane, line)
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
 	-- Key table for moving tabs around
 	{ key = "m", mods = "LEADER", action = act.ActivateKeyTable({ name = "move_tab", one_shot = false }) },
+	-- Or shortcuts to move tab w/o move_tab table. SHIFT is for when caps lock is on
+	{ key = "{", mods = "LEADER|SHIFT", action = act.MoveTabRelative(-1) },
+	{ key = "}", mods = "LEADER|SHIFT", action = act.MoveTabRelative(1) },
 
 	-- Lastly, workspace
 	{ key = "w", mods = "LEADER", action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) },
@@ -90,44 +116,69 @@ config.key_tables = {
 -- I don't like the look of "fancy" tab bar
 config.use_fancy_tab_bar = false
 config.status_update_interval = 1000
-wezterm.on("update-right-status", function(window, pane)
+config.tab_bar_at_bottom = false
+wezterm.on("update-status", function(window, pane)
 	-- Workspace name
 	local stat = window:active_workspace()
+	local stat_color = "#f7768e"
 	-- It's a little silly to have workspace name all the time
 	-- Utilize this to display LDR or current key table name
 	if window:active_key_table() then
 		stat = window:active_key_table()
+		stat_color = "#7dcfff"
 	end
 	if window:leader_is_active() then
 		stat = "LDR"
+		stat_color = "#bb9af7"
 	end
 
 	-- Current working directory
 	local basename = function(s)
-		-- Nothign a little regex can't fix
+		-- Nothing a little regex can't fix
 		return string.gsub(s, "(.*[/\\])(.*)", "%2")
 	end
-	local cwd = basename(pane:get_current_working_dir())
+	-- CWD and CMD could be nil (e.g. viewing log using Ctrl-Alt-l). Not a big deal, but check in case
+	local cwd = pane:get_current_working_dir()
+	cwd = cwd and basename(cwd) or ""
 	-- Current command
-	local cmd = basename(pane:get_foreground_process_name())
+	local cmd = pane:get_foreground_process_name()
+	cmd = cmd and basename(cmd) or ""
 
 	-- Time
 	local time = wezterm.strftime("%H:%M")
 
-	-- Let's add color to one of the components
+	-- Left status (left of the tab line)
+	window:set_left_status(wezterm.format({
+		{ Foreground = { Color = stat_color } },
+		{ Text = "  " },
+		{ Text = wezterm.nerdfonts.oct_table .. "  " .. stat },
+		{ Text = " |" },
+	}))
+
+	-- Right status
 	window:set_right_status(wezterm.format({
 		-- Wezterm has a built-in nerd fonts
-		{ Text = wezterm.nerdfonts.oct_table .. "  " .. stat },
-		{ Text = " | " },
+		-- https://wezfurlong.org/wezterm/config/lua/wezterm/nerdfonts.html
 		{ Text = wezterm.nerdfonts.md_folder .. "  " .. cwd },
 		{ Text = " | " },
-		{ Foreground = { Color = "FFB86C" } },
+		{ Foreground = { Color = "#e0af68" } },
 		{ Text = wezterm.nerdfonts.fa_code .. "  " .. cmd },
 		"ResetAttributes",
 		{ Text = " | " },
 		{ Text = wezterm.nerdfonts.md_clock .. "  " .. time },
-		{ Text = " |" },
+		{ Text = "  " },
 	}))
 end)
+
+--[[ Appearance setting for when I need to take pretty screenshots
+config.enable_tab_bar = false
+config.window_padding = {
+  left = '0.5cell',
+  right = '0.5cell',
+  top = '0.5cell',
+  bottom = '0cell',
+
+}
+--]]
 
 return config
