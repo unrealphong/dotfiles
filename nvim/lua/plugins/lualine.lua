78bc222c8acc21e3019b76240d7b227b19e06c56
phonglenus <hongphongle115@gmail.com>
2024-01-01 05:00:31 +0700

    use rose-pine colorscheme

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,6 +2,9 @@ return {
   'nvim-lualine/lualine.nvim',
   config = function()
     require('lualine').setup {
+      options = {
+        theme = 'auto',
+      },
       sections = {
         lualine_a = { 'mode' },
         lualine_b = { { 'filename', path = 4 } },
