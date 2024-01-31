6fa722078c6e6a05a364973701d73ef74e7601c8
Hong-Phong Le <hongphongle115@gmail.com>
2024-02-01 06:34:14 +0700

    update(zsh): update func set github

--- fish/config.fish
+++ fish/config.fish
@@ -63,5 +63,5 @@ zoxide init fish | source
 # uninstall by removing these lines
 [ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
 
-starship init fish | source
+# starship init fish | source
 
