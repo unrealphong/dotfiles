f24c98f3d57a27c5522c3da6796c5f4b79391b28
unrealphong <hongphongle115@gmail.com>
2024-02-20 01:08:32 +0700

    update config wezterm

---
+++ wezterm/config/font.lua
@@ -0,0 +1,7 @@
+local wezterm = require("wezterm")
+
+return {
+	font = wezterm.font("JetBrainsMono NF"),
+	font_size = 14,
+	freetype_load_flags = "NO_HINTING", -- smoother font
+}
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,43 +1,10 @@
 local wezterm = require("wezterm")
--- local colors = require("themes/rose-pine").colors()
+local Config = require("config")
 
-return {
+require("events.tab-title").setup()
+require("events.right-status").setup()
 
-	color_scheme = "Everforest Dark (Gogh)",
-	font = wezterm.font("GeistMono Nerd Font", { weight = "Medium", stretch = "Normal", style = "Normal" }),
-
-	font_rules = {
-		{
-			italic = true,
-			font = wezterm.font("JetBrainsMono NF", { weight = "Medium", stretch = "Normal", style = "Italic" }),
-		},
-		{
-			intensity = "Bold",
-			font = wezterm.font("JetBrainsMono NF", { weight = "Bold", stretch = "Normal", style = "Normal" }),
-		},
-		{
-			intensity = "Bold",
-			italic = true,
-			font = wezterm.font("JetBrainsMono NF", { weight = "Bold", stretch = "Normal", style = "Italic" }),
-		},
-	},
-
-	font_size = 16,
-
-	colors = colors,
-	initial_rows = 40,
-	initial_cols = 120,
-	window_padding = {
-		left = 15,
-		right = 15,
-		top = 10,
-		bottom = 10,
-	},
-
-	window_background_opacity = 0.92,
-	window_decorations = "RESIZE",
-	enable_tab_bar = false,
-	scrollback_lines = 5000,
-	enable_scroll_bar = false,
-	check_for_updates = false,
-}
+return Config:init()
+	:append(require("config.font"))
+	:append(require("config.maps"))
+	:append(require("config.appearance")).options
