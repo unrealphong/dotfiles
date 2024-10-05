bd13aa80083e57e3a164a01c2179aa37ede2d917
unrealphong <hongphongle115@gmail.com>
2024-10-05 11:26:18 +0700

    add fish shell config

---
+++ config/fish/config.fish
@@ -0,0 +1,22 @@
+set -gx PATH /opt/homebrew/bin $PATH
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    starship init fish | source
+end
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/homebrew/Caskroom/miniforge/base/bin/conda
+    eval /opt/homebrew/Caskroom/miniforge/base/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/opt/homebrew/Caskroom/miniforge/base/etc/fish/conf.d/conda.fish"
+        . "/opt/homebrew/Caskroom/miniforge/base/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/opt/homebrew/Caskroom/miniforge/base/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
